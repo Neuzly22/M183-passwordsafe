2b3e3061cbde2b6e0dc8c719b4bf9fbb6b0f0772
Noah <noah.leo@hispeed.ch>
2021-06-22 11:47:14 +0200

    [FEATURE]inital commit

---
+++ src/test/java/ch/bbw/passwordSafe/PasswordSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.passwordSafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
